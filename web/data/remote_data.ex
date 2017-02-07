5832e4cad9dfdb488fffc228ac1485f9ee24dcf5
robmckinnon <rob@movingflow>
2017-02-07 13:54:12 +0000

    Retrieve data from file when offline
    When unable to retrieve data via HTTP, read data from local filesystem.

--- web/data/remote_data.ex
+++ web/data/remote_data.ex
@@ -8,7 +8,8 @@ defmodule RemoteData do
 
   def data_list register, data_file do
     url = "https://raw.githubusercontent.com/openregister/#{data_file}"
-    ConCache.get_or_store(:my_cache, register, get_list(url, register))
+    file_path = "../#{data_file}" |> String.replace("/master","")
+    ConCache.get_or_store(:my_cache, register, get_list(url, file_path, register))
   end
 
   def maps_list maps_path do
@@ -22,26 +23,42 @@ defmodule RemoteData do
     ConCache.get_or_store(:my_cache, "index", get_maps_index(maps_path))
   end
 
-  defp maps_url file, maps_path do
-    "https://raw.githubusercontent.com/openregister/#{maps_path}/#{file}"
+  defp maps_url_and_path file, maps_path do
+    url = "https://raw.githubusercontent.com/openregister/#{maps_path}/#{file}"
+    file_path = "../#{maps_path}/#{file}" |> String.replace("/master","")
+    [url, file_path]
   end
 
   defp map_list file, maps_path do
-    url = maps_url "#{file}.tsv", maps_path
-    ConCache.get_or_store(:my_cache, file, get_list(url, file))
+    [url, file_path] = maps_url_and_path("#{file}.tsv", maps_path)
+    ConCache.get_or_store(:my_cache, file, get_list(url, file_path, file))
   end
 
   defp get_maps_index maps_path do
     fn () ->
-      url = maps_url "index.yml", maps_path
-      HTTPoison.get!(url).body
+      [url, file_path] = maps_url_and_path("index.yml", maps_path)
+      url
+      |> retrieve_data(file_path)
       |> YamlElixir.read_from_string
     end
   end
 
-  defp get_list url, kind do
-    fn () ->
+  defp retrieve_data url, file_path do
+    try do
+      IO.puts url
       HTTPoison.get!(url).body
+    rescue
+      HTTPoison.Error ->
+        IO.puts file_path
+        {:ok, body} = File.read(file_path)
+        body
+    end
+  end
+
+  defp get_list url, file_path, kind do
+    fn () ->
+      url
+      |> retrieve_data(file_path)
       |> String.replace_trailing("\n", "")
       |> DataMorph.structs_from_tsv(OpenRegister, kind)
       |> Enum.to_list
