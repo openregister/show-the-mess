333c51718dc4c833bb1ec07c163ea97e385a9876
robmckinnon <rob@movingflow>
2017-02-07 15:05:25 +0000

    Add data summary table view
    Render list name, description, count, and field names.

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,22 @@
 defmodule ShowTheMess.PageView do
   use ShowTheMess.Web, :view
+
+  def key primary_key do
+    primary_key |> String.replace("-","_") |> String.to_atom()
+  end
+
+  def fields entities, primary_key do
+    map = entities |> List.first
+    if map do
+      key = key(primary_key)
+      map
+      |> Map.keys
+      |> Enum.filter(&(&1 != :__struct__ and &1 != key))
+      |> Enum.sort
+      |> Enum.join(" ")
+    else
+      ""
+    end
+  end
+
 end
