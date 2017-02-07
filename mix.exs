234c279ce9c5f93631136d3e8ba9bbf4cc69874a
robmckinnon <rob@movingflow>
2017-02-07 11:29:55 +0000

    Add httpoison and con_cache to applications list

--- mix.exs
+++ mix.exs
@@ -19,7 +19,11 @@ defmodule ShowTheMess.Mixfile do
   def application do
     [mod: {ShowTheMess, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex,
+                    :httpoison,
+                    :con_cache,
+                   ]
+    ]
   end
 
   # Specifies which paths to compile per environment.
