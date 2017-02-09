4c765576f777cb605bf25b7dbd9104dbd869b0b6
robmckinnon <rob@movingflow>
2017-02-09 17:24:13 +0000

    Put list of links to data reports on homepage
    Hardcode the parameters required for prison and local-authority-eng in the link hrefs.

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,19 +2,24 @@ defmodule ShowTheMess.PageController do
   use ShowTheMess.Web, :controller
 
   def index(conn, params) do
-    register = params |> Map.get("register", "prison")
-    data_file = params |> Map.get("data", "prison-data/master/data/discovery/prison/prisons.tsv")
-    maps_path = params |> Map.get("maps", "prison-data/master/maps")
+    register = params |> Map.get("register")
 
-    data_list = RemoteData.data_list(register, data_file)
-    maps_list = RemoteData.maps_list(maps_path, register)
-    maps_index = RemoteData.maps_index(maps_path)
-    render conn, "index.html",
-      data_list: data_list,
-      data_list_by_id: data_list |> Enum.group_by(& &1 |> Map.get(String.to_atom(register))),
-      maps_list: maps_list,
-      maps_index: maps_index,
-      register: register
+    if is_nil(register) do
+      render conn, "index.html", register: nil
+    else
+      data_file = params |> Map.get("data")
+      maps_path = params |> Map.get("maps")
+
+      data_list = RemoteData.data_list(register, data_file)
+      maps_list = RemoteData.maps_list(maps_path, register)
+      maps_index = RemoteData.maps_index(maps_path)
+      render conn, "index.html",
+        data_list: data_list,
+        data_list_by_id: data_list |> Enum.group_by(& &1 |> Map.get(String.to_atom(register))),
+        maps_list: maps_list,
+        maps_index: maps_index,
+        register: register
+    end
   end
 
   def clear_cache(conn, _params) do
